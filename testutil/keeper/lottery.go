cd83c5d23ea4ca3c1382defe3c5043d95f6bd568
Andrea Giacobino <no.andrea@gmail.com>
2023-02-03 18:53:39 +0100

    temporary disable testing for msg_server

--- testutil/keeper/lottery.go
+++ testutil/keeper/lottery.go
@@ -1,53 +1,63 @@
 package keeper
 
 import (
+	"lottery/x/lottery/keeper"
 	"testing"
 
-	"lottery/x/lottery"
-	"lottery/x/lottery/keeper"
-
-	"github.com/cosmos/cosmos-sdk/codec"
-	codectypes "github.com/cosmos/cosmos-sdk/codec/types"
-	"github.com/cosmos/cosmos-sdk/store"
-	storetypes "github.com/cosmos/cosmos-sdk/store/types"
+	// "lottery/x/lottery"
+	// "lottery/x/lottery/keeper"
+	// "github.com/cosmos/cosmos-sdk/codec"
+	// codectypes "github.com/cosmos/cosmos-sdk/codec/types"
+	// "github.com/cosmos/cosmos-sdk/store"
+	// storetypes "github.com/cosmos/cosmos-sdk/store/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	typesparams "github.com/cosmos/cosmos-sdk/x/params/types"
-	"github.com/stretchr/testify/require"
-	"github.com/tendermint/tendermint/libs/log"
-	tmproto "github.com/tendermint/tendermint/proto/tendermint/types"
-	tmdb "github.com/tendermint/tm-db"
+	"github.com/tendermint/tendermint/proto/tendermint/types"
+	// typesparams "github.com/cosmos/cosmos-sdk/x/params/types"
+	// "github.com/stretchr/testify/require"
+	// "github.com/tendermint/tendermint/libs/log"
+	// tmproto "github.com/tendermint/tendermint/proto/tendermint/types"
+	// tmdb "github.com/tendermint/tm-db"
+	// banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
+	// paramtypes "github.com/cosmos/cosmos-sdk/x/params/types"
 )
 
 func LotteryKeeper(t testing.TB) (*keeper.Keeper, sdk.Context) {
-	storeKey := sdk.NewKVStoreKey(lottery.StoreKey)
-	memStoreKey := storetypes.NewMemoryStoreKey(lottery.MemStoreKey)
+	// storeKey := sdk.NewKVStoreKey(lottery.StoreKey)
+	// memStoreKey := storetypes.NewMemoryStoreKey(lottery.MemStoreKey)
 
-	db := tmdb.NewMemDB()
-	stateStore := store.NewCommitMultiStore(db)
-	stateStore.MountStoreWithDB(storeKey, sdk.StoreTypeIAVL, db)
-	stateStore.MountStoreWithDB(memStoreKey, sdk.StoreTypeMemory, nil)
-	require.NoError(t, stateStore.LoadLatestVersion())
+	// bankStoreKey := sdk.NewKVStoreKey(banktypes.StoreKey)
 
-	registry := codectypes.NewInterfaceRegistry()
-	cdc := codec.NewProtoCodec(registry)
+	// paramsStoreKey := sdk.NewKVStoreKey(paramtypes.StoreKey)
+	// paramsStoreMemKey := sdk.NewKVStoreKey(paramtypes.TStoreKey)
 
-	paramsSubspace := typesparams.NewSubspace(cdc,
-		lottery.Amino,
-		storeKey,
-		memStoreKey,
-		"LotteryParams",
-	)
-	k := keeper.NewKeeper(
-		cdc,
-		storeKey,
-		memStoreKey,
-		paramsSubspace,
-	)
+	// db := tmdb.NewMemDB()
+	// stateStore := store.NewCommitMultiStore(db)
+	// stateStore.MountStoreWithDB(storeKey, sdk.StoreTypeIAVL, db)
+	// stateStore.MountStoreWithDB(memStoreKey, sdk.StoreTypeMemory, nil)
+	// require.NoError(t, stateStore.LoadLatestVersion())
 
-	ctx := sdk.NewContext(stateStore, tmproto.Header{}, false, log.NewNopLogger())
+	// registry := codectypes.NewInterfaceRegistry()
+	// cdc := codec.NewProtoCodec(registry)
 
-	// Initialize params
-	k.SetParams(ctx, lottery.DefaultParams())
+	// paramsSubspace := typesparams.NewSubspace(cdc,
+	// 	lottery.Amino,
+	// 	storeKey,
+	// 	memStoreKey,
+	// 	"LotteryParams",
+	// )
+	// k := keeper.NewKeeper(
+	// 	cdc,
+	// 	storeKey,
+	// 	memStoreKey,
+	// 	paramsSubspace,
 
-	return k, ctx
+	// )
+
+	// ctx := sdk.NewContext(stateStore, tmproto.Header{}, false, log.NewNopLogger())
+
+	// // Initialize params
+	// k.SetParams(ctx, lottery.DefaultParams())
+
+	// return k, ctx
+	return nil, sdk.NewContext(nil, types.Header{}, false, nil)
 }
--- x/lottery/keeper/msg_server_test.go
+++ x/lottery/keeper/msg_server_test.go
@@ -2,16 +2,49 @@ package keeper_test
 
 import (
 	"context"
-	"testing"
-
 	keepertest "lottery/testutil/keeper"
 	"lottery/x/lottery"
 	"lottery/x/lottery/keeper"
+	"testing"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/stretchr/testify/require"
 )
 
 func setupMsgServer(t testing.TB) (lottery.MsgServer, context.Context) {
 	k, ctx := keepertest.LotteryKeeper(t)
 	return keeper.NewMsgServerImpl(*k), sdk.WrapSDKContext(ctx)
+}
+
+func Test_msgServer_PlaceBet(t *testing.T) {
+
+	msgServer, ctx := setupMsgServer(t)
+
+	tests := []struct {
+		name    string
+		msg     *lottery.MsgPlaceBetRequest
+		wantErr error
+	}{
+		{
+			"OK: place-bet successful",
+			&lottery.MsgPlaceBetRequest{
+				Amount: sdk.NewInt64Coin(sdk.DefaultBondDenom, 10_000_000),
+				Signer: "lot1k6g24mg4l5ktpyzt0qd8lmcp27d7u9yj76l6vx",
+			},
+			nil,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+
+			_, err := msgServer.PlaceBet(ctx, tt.msg)
+
+			if tt.wantErr == nil {
+				require.NoError(t, err)
+			} else {
+				require.Error(t, err)
+				require.Equal(t, tt.wantErr.Error(), err.Error())
+			}
+		})
+	}
 }
